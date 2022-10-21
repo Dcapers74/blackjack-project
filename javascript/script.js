a985e359d04ee20377a6b14d44c6b4005e1bc0c5
Dcaper74# <dcapers.prsvr@gmail.com>
2022-10-20 22:05:13 -0400

    almost complete

---
+++ javascript/script.js
@@ -0,0 +1,23 @@
+let firstCard = 10
+let secondCard = 11
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+
+if (sum <= 20){
+    message = "Do you want to draw a new card?"
+}else if(sum === 21){
+    message = "Wohoo! You've got Blackjack!"
+
+}else {
+    message = "you're out of the game!"
+    isAlive = false
+}
+
+
+console.log(message)
+
+function startGame(){
+    
+}
