c35f31c5ecfb1bedeef22bc6b20d7e76b609c715
Dcaper74# <dcapers.prsvr@gmail.com>
2022-10-25 22:10:22 -0400

    tuesday completion

--- javascript/script.js
+++ javascript/script.js
@@ -1,23 +1,28 @@
 let firstCard = 10
-let secondCard = 11
+let secondCard = 4
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 
-if (sum <= 20){
-    message = "Do you want to draw a new card?"
-}else if(sum === 21){
-    message = "Wohoo! You've got Blackjack!"
 
-}else {
-    message = "you're out of the game!"
-    isAlive = false
-}
-
-
-console.log(message)
+let messageEl = document.getElementById("message-el")
+let sumEl  = document.getElementById("sum-el")
+let cardsEl  = document.getElementById("cards-el")
 
 function startGame(){
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + firstCard + secondCard
+    if (sum <= 20){
+        message = "Do you want to draw a new card?"
+    }else if(sum === 21){
+        message = "Wohoo! You've got Blackjack!"
     
+    }else {
+        message = "you're out of the game!"
+        isAlive = false
+    }
+  messageEl.textContent= message
+  
 }
+
